feab038a416548b518f5228919325c4fb8a9bdd9
Luciano Amâncio <madruga665@hotmail.com>
2021-12-17 23:38:02 -0300

    refactor:  fetch dos dados dos pokemons

--- src/scripts/pokemonCardScript.ts
+++ src/scripts/pokemonCardScript.ts
@@ -4,31 +4,32 @@ import Pokemon from "../../classes/Pokemon";
 const pokemonsData = new FetchData();
 const pokemonsSection = <HTMLDivElement>document.querySelector(".pokemons-cards-container");
 
-pokemonsData.getData("https://pokeapi.co/api/v2/pokemon/").then((data) =>
-  data.results.map((data: any) => {
-    pokemonsData.getData(data.url).then((data) => {
-      const pokemon = new Pokemon(
-        data.name,
-        data.types[0].type.name,
-        data.sprites.other.dream_world.front_default
-      );
-      const card = document.createElement("div");
-      card.classList.add("card-container");
-      card.classList.add("pokemon-card");
-      pokemonsSection.appendChild(card);
+window.onload = async () => {
+  const data = await pokemonsData.getData("https://pokeapi.co/api/v2/pokemon/");
+  data.results.map(async (data: any) => {
+    const pokemonDetails = await pokemonsData.getData(data.url);
+    const pokemon = new Pokemon(
+      pokemonDetails.name,
+      pokemonDetails.types[0].type.name,
+      pokemonDetails.sprites.other.dream_world.front_default
+    );
 
-      const cardImg = document.createElement("img");
-      cardImg.style.width = "50px";
-      cardImg.src = pokemon.picture;
-      card.appendChild(cardImg);
+    const card = document.createElement("div");
+    card.classList.add("card-container");
+    card.classList.add("pokemon-card");
+    pokemonsSection.appendChild(card);
 
-      const cardName = document.createElement("h3");
-      cardName.innerText = pokemon.name;
-      card.appendChild(cardName);
+    const cardImg = document.createElement("img");
+    cardImg.style.width = "50px";
+    cardImg.src = pokemon.picture;
+    card.appendChild(cardImg);
 
-      const cardType = document.createElement("p");
-      cardType.innerText = pokemon.type;
-      card.appendChild(cardType);
-    });
-  })
-);
+    const cardName = document.createElement("h3");
+    cardName.innerText = pokemon.name;
+    card.appendChild(cardName);
+
+    const cardType = document.createElement("p");
+    cardType.innerText = pokemon.type;
+    card.appendChild(cardType);
+  });
+};
